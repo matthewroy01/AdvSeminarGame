d281f440d493203692a6cf98c2527f6ac320bb1f
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2017-09-14 17:35:14 -0400

    organization in Enemy class and others

---
+++ Project/SilentRealm/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    // the direction the enemy is facing
+    public enum Dirs { up = 0, down = 1, left = 2, right = 3 };
+
+	public virtual void Step()
+	{
+		Debug.Log("ENEMYSTEP - the virtual function was called");
+	}
+
+    public virtual void Panic(bool panicing)
+    {
+        Debug.Log("ENEMYPANIC - the virtual function was called");
+    }
+}
--- Project/SilentRealm/Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Project/SilentRealm/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class EnemyPatrol : Enemy {
 
-	[SerializeField] private bool patrolMode = true;
-	public string currentDirection;
+    [SerializeField] private bool patrolMode = true;
+	public Dirs currentDirection;
 
-    // this is serialized so it can appear in the Inspector!
+    // this is serialized so it can appear in the Inspector
     [Header("List of points for patrolling")]
     public PatrolPoint[] points;
 
-void Start () {
+    void Start () {
 		
 	}
 
 	void Update ()
 	{
-        CheckPoints();
+        
 	}
 
     private void CheckPoints()
@@ -33,33 +33,54 @@ void Start () {
 
 	public override void Step()
 	{
-		Debug.Log("ENEMYPATROL - the overriden function was called");
-		// switch direction each time as a test
+        // in patrol mode, move according to currentDirection
 		if (patrolMode)
 		{
-			if (currentDirection == "up")
+			if (currentDirection == Dirs.up)
 			{
 				transform.position = new Vector2(transform.position.x, transform.position.y + 1);
 			}
-			else if (currentDirection == "down")
+			else if (currentDirection == Dirs.down)
 			{
 				transform.position = new Vector2(transform.position.x, transform.position.y - 1);
 			}
-			else if (currentDirection == "left")
+			else if (currentDirection == Dirs.left)
 			{
 				transform.position = new Vector2(transform.position.x - 1, transform.position.y);
 			}
-			else if (currentDirection == "right")
+			else if (currentDirection == Dirs.right)
 			{
 				transform.position = new Vector2(transform.position.x + 1, transform.position.y);
 			}
 		}
+
+        // outside of patrol mode, path towards the player
+        if (!patrolMode)
+        {
+            // figure out A* algorithm
+        }
+
+        // check to see if this enemy has reached a point in its array
+        CheckPoints();
 	}
+
+    public override void Panic(bool panicing)
+    {
+        if (panicing)
+        {
+            patrolMode = false;
+        }
+        else
+        {
+            patrolMode = true;
+        }
+    }
 }
 
+// struct of points that contains where the enemy should turn and in what direction
 [System.Serializable]
 public struct PatrolPoint
 {
     public Vector2 point;
-    public string direction;
+    public Enemy.Dirs direction;
 }
