a1df542dd809977e67ece1271d0860590bc31f49
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2017-09-14 17:47:58 -0400

    reorganized vectors and directions in PlayerMovement

--- Project/SilentRealm/Assets/Scripts/Player/PlayerMovement.cs
+++ Project/SilentRealm/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	[SerializeField] private GameObject gameManager = null;
+    public enum Dirs { up = 0, down = 1, left = 2, right = 3 };
+
+    [SerializeField] private GameObject gameManager = null;
 	public LayerMask wallLayer;
 
+    public Vector2 vUp, vDown, vLeft, vRight;
+
 	void Start () {
+        // find the GameManager
 		gameManager = GameObject.Find("GameManager");
-	}
+        // initialize the movement vectors
+        UpdateVectors();
+    }
 
 	void Update () {
 		// movement
@@ -18,59 +25,79 @@ public class PlayerMovement : MonoBehaviour {
 
 	private void Movement()
 	{
-		if (Input.GetButtonDown("Up") && checkMov("up"))
+		if (Input.GetButtonDown("Up") && checkMov(Dirs.up))
 		{
-			transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+            // update the vectors each time movement occurs
+            UpdateVectors();
+
+            transform.position = new Vector2(transform.position.x, transform.position.y + 1);
 			gameManager.GetComponent<UtilityBroadcast>().togetherNow();
 		}
-		if (Input.GetButtonDown("Down") && checkMov("down"))
+		if (Input.GetButtonDown("Down") && checkMov(Dirs.down))
 		{
-			transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+            // update the vectors each time movement occurs
+            UpdateVectors();
+
+            transform.position = new Vector2(transform.position.x, transform.position.y - 1);
 			gameManager.GetComponent<UtilityBroadcast>().togetherNow();
 		}
-		if (Input.GetButtonDown("Left") && checkMov("left"))
+		if (Input.GetButtonDown("Left") && checkMov(Dirs.left))
 		{
-			transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+            // update the vectors each time movement occurs
+            UpdateVectors();
+
+            transform.position = new Vector2(transform.position.x - 1, transform.position.y);
 			gameManager.GetComponent<UtilityBroadcast>().togetherNow();
 		}
-		if (Input.GetButtonDown("Right") && checkMov("right"))
+		if (Input.GetButtonDown("Right") && checkMov(Dirs.right))
 		{
-			transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+            // update the vectors each time movement occurs
+            UpdateVectors();
+
+            transform.position = new Vector2(transform.position.x + 1, transform.position.y);
 			gameManager.GetComponent<UtilityBroadcast>().togetherNow();
 		}
 	}
 
-	private bool checkMov (string dir)
+	private bool checkMov (Dirs dir)
 	{
 		// if a wall is detected, movement is impossible
-		if (dir == "up")
+		if (dir == Dirs.up)
 		{
-			if (!Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y + 1), 0.2f, wallLayer))
+			if (!Physics2D.OverlapCircle(vUp, 0.2f, wallLayer))
 			{
 				return true;
 			}
 		}
-		else if (dir == "down")
+		else if (dir == Dirs.down)
 		{
-			if (!Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 1), 0.2f, wallLayer))
+			if (!Physics2D.OverlapCircle(vDown, 0.2f, wallLayer))
 			{
 				return true;
 			}
 		}
-		else if (dir == "left")
+		else if (dir == Dirs.left)
 		{
-			if (!Physics2D.OverlapCircle(new Vector2(transform.position.x - 1, transform.position.y), 0.2f, wallLayer))
+			if (!Physics2D.OverlapCircle(vLeft, 0.2f, wallLayer))
 			{
 				return true;
 			}
 		}
-		else if (dir == "right")
+		else if (dir == Dirs.right)
 		{
-			if (!Physics2D.OverlapCircle(new Vector2(transform.position.x + 1, transform.position.y), 0.2f, wallLayer))
+			if (!Physics2D.OverlapCircle(vRight, 0.2f, wallLayer))
 			{
 				return true;
 			}
 		}
 		return false;
 	}
+
+    private void UpdateVectors()
+    {
+        vUp = new Vector2(transform.position.x, transform.position.y + 1);
+        vDown = new Vector2(transform.position.x, transform.position.y - 1);
+        vLeft = new Vector2(transform.position.x - 1, transform.position.y);
+        vRight = new Vector2(transform.position.x + 1, transform.position.y);
+    }
 }
