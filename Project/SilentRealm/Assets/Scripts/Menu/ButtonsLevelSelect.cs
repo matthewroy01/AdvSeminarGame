f4acda52402687141bc5a6d16199fd7e45ec794c
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2017-10-13 15:36:02 -0400

    added levels 1 and 2

---
+++ Project/SilentRealm/Assets/Scripts/Menu/ButtonsLevelSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsLevelSelect : MonoBehaviour {
+
+	void Update () {
+		if (GetComponent<MenuNavigation>().isActive)
+		{
+			if (Input.GetButtonDown("Select"))
+			{
+				switch (GetComponent<MenuNavigation>().currentSelection)
+				{
+					case 0 :
+						Debug.Log("level 4");
+						break;
+
+					case 1 :
+						Debug.Log("level 3");
+						break;
+
+					case 2 :
+						Debug.Log("level 2");
+						SceneManager.LoadScene(2);
+						break;
+
+					case 3 :
+						Debug.Log("level 1");
+						SceneManager.LoadScene(1);
+						break;
+				}
+			}
+		}
+	}
+}
